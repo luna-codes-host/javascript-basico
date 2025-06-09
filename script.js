0041846a1e7aaf3a81c97674d28d4a41ec5df9f1
Alenisse Luna <alunam2@est.ups.edu.ec>
2025-06-09 16:53:25 -0500

    Arreglos, bucles y condicionales

--- script.js
+++ script.js
@@ -3,3 +3,28 @@
 function mostrarMensaje(){
     alert("Hola mundo :3 ");
 }
+
+function imprimirVariables(){
+    let nombre ="Alenisse";
+    let edad = 21;
+    //Forma tradicional de concatenar cadenas
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+    console.error("Error: bla bla bla");
+    console.warn("Advertencia: x x x")
+    //Forma moderna
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+}
+
+function arreglo(){
+    //areglo
+    let frutas = ['manzana','pera', 'banana']
+    for(let f of frutas){
+        console.log(f)
+    }
+    if (frutas[1] === 'pera'){
+        console.log("La segunda fruta es una pera");
+    } else{
+        console.log('no es una pera');
+    }
+}
